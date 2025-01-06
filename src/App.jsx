a7b7335558a115e32496da02cb7608bed89fea10
Krzysztof W <krzysztof.wylot@gmail.com>
2025-01-06 15:11:31 +0100

    update 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
-export default function App() {
+import React from 'react'
+
+    export default function App() {
       return (
         <div>
-          <h1>Hello, React!</h1>
+          <h1>Hello, React!2</h1>
         </div>
       )
     }
